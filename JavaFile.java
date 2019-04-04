53ffa9160ffd590d0472cff1bb69c52f2feb79a3
Ruba Mohmmad <ruba17436@gmail.com>
2019-04-04 09:26:01 +0200

    Add java File

--- JavaFile.java
+++ JavaFile.java
@@ -2,6 +2,6 @@ public class JavaFile {
 int x=5;
 public JavaFile (){
 super();
-x=45;}
+x=44;}
 public void print (){
 system.out.println("java");}}
---
+++ javaFile (2).java
@@ -0,0 +1,7 @@
+public class JavaFile {
+int x=7;
+public JavaFile (){
+super();
+x=60;}
+public void print (){
+system.out.println("java");}}
